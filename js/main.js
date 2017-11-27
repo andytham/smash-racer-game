2d51bebad495c9f0364ea086b08694337b8e97ae
andy tham <thamandyy@gmail.com>
2017-11-27 18:16:53 -0500

    kirby and link running

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("start");
+button.addEventListener("click", function(){
+  document.getElementById("gamePage").style.display = "block";
+  document.getElementById("landingPage").style.display = "none";
+
+  document.getElementById("restart").style.display = "block";
+  }
+);
