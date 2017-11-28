a1daa9c5c129197399f94c3399bbc371555920e9
andy tham <thamandyy@gmail.com>
2017-11-28 17:19:13 -0500

    added dom javascript to move players

--- js/main.js
+++ js/main.js
@@ -16,5 +16,23 @@ button2.addEventListener("click", function(){
   }
 );
 
-// const playerOne = document.get elementById('playerOne');
-// console.log(playerOne.style.left = "100px");
+let playerOne = document.getElementById("link");
+let playerTwo = document.getElementById("kirby");
+let moveVarOne = 0;
+let moveVarTwo = 0;
+
+function movePlayers(event) {
+  console.log(event)
+  console.log(playerOne.style.left);
+  if(event.keyCode == 90){
+    moveVarOne = moveVarOne + 4;
+    playerOne.style.left = moveVarOne + "%";
+  }
+  if(event.keyCode == 39){
+    moveVarTwo = moveVarTwo + 4;
+    playerTwo.style.left = moveVarTwo + "%";
+  }
+}
+
+
+document.addEventListener("keydown", movePlayers);
