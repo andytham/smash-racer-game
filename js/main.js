d816ca3f39efd3d1a67b2a27db37ef7b4c197c3c
andy tham <thamandyy@gmail.com>
2017-11-28 21:21:31 -0500

    added win count

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ let runAnim = 0;
 //makes sure no one cheats before starting
 let startCheck = 0;
 
+let winCondition = 0;
+let kirbyWinCount = 0;
+let linkWinCount = 0;
+
 //start button to initialize game page
 let button = document.getElementById("start");
 button.addEventListener("click", function(){
@@ -31,6 +35,7 @@ button2.addEventListener("click", function(){
   document.getElementById("kirbyWin").style.display = "none";
   document.getElementById("linkWin").style.display = "none";
   startCheck = 0;
+  winCondition = 0;
   }
 );
 
@@ -56,7 +61,7 @@ function runningKirby(){
   kirby.src = "images/kirby_1.gif";
   kirby.style.left= "0%";
   kirby.style.width= "18%";
-  kirby.style.bottom= "-2%";
+  kirby.style.bottom= "3%";
 }
 
 function standingKirby(){
@@ -69,6 +74,10 @@ function standingKirby(){
 // the whole game function, checks for standing animation and changes it, then moves the players
 
 function movePlayers(event) {
+  //only do something if z or right arrow pressed
+  if(event.keyCode !== 90 && event.keyCode !== 39){
+    return;
+  }
   if( startCheck == 0){
     return;
   }
@@ -79,10 +88,24 @@ function movePlayers(event) {
   }
   if(moveVarOne > 83 || moveVarTwo > 80){
       if(moveVarOne > 83){
+        if(winCondition == 0){
+          linkWinCount = linkWinCount + 1;
+        }
+        console.log(linkWinCount);
+        document.getElementsByClassName('linkCount')[0].innerHTML = linkWinCount;
+        document.getElementsByClassName('kirbyCount')[0].innerHTML = kirbyWinCount;
         document.getElementById("linkWin").style.display = "block";
+        winCondition = 1;
+
       }
       if(moveVarTwo > 80){
+        if(winCondition == 0){
+          kirbyWinCount = kirbyWinCount + 1;
+        }
+        document.getElementsByClassName('linkCount')[1].innerHTML = linkWinCount;
+        document.getElementsByClassName('kirbyCount')[1].innerHTML = kirbyWinCount;
         document.getElementById("kirbyWin").style.display = "block";
+        winCondition = 1;
       }
       return;
 
