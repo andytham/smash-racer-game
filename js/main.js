1295bff00bfdb4e024494c703f58002c1a619954
andy tham <thamandyy@gmail.com>
2017-11-28 20:07:31 -0500

    almost done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+let runAnim = 0;
 let button = document.getElementById("start");
 button.addEventListener("click", function(){
   document.getElementById("gamePage").style.display = "block";
@@ -5,32 +6,83 @@ button.addEventListener("click", function(){
 
   document.getElementById("restart").style.display = "block";
   }
+
 );
 
 let button2 = document.getElementById("restart");
 button2.addEventListener("click", function(){
   document.getElementById("gamePage").style.display = "none";
   document.getElementById("landingPage").style.display = "block";
-
   document.getElementById("restart").style.display = "none";
+
+  moveVarOne = 0;
+  moveVarTwo = 0;
+  kirby.style.left = 0;
+  standingLink();
+  runAnim = 0;
+  document.getElementById("kirbyWin").style.display = "none";
+  document.getElementById("linkWin").style.display = "none";
   }
 );
 
-let playerOne = document.getElementById("link");
-let playerTwo = document.getElementById("kirby");
+let link = document.getElementById("link");
+let kirby = document.getElementById("kirby");
 let moveVarOne = 0;
 let moveVarTwo = 0;
 
+function runningLink(){
+  link.src = "images/link_1.gif";
+  link.style.left = "2%";
+  link.style.width = "22%";
+}
+function standingLink(){
+  link.src = "images/link_0.gif";
+  link.style.left = "2%";
+  link.style.width = "14%";
+}
+
+function runningKirby(){
+  kirby.src = "images/kirby_1.gif";
+  kirby.style.left= "0%";
+  kirby.style.width= "18%";
+  kirby.style.bottom= "-2%";
+}
+
+function standingKirby(){
+  kirby.src = "images/kirby_0.png";
+  kirby.style.left= "3%";
+  kirby.style.width= "10%";
+  kirby.style.bottom= "11%";
+}
+
 function movePlayers(event) {
-  console.log(event)
-  console.log(playerOne.style.left);
+  if (runAnim == 0) {
+    runningKirby();
+    runningLink();
+    runAnim = 1;
+  }
+  if(moveVarOne > 83 || moveVarTwo > 80){
+      if(moveVarOne > 83){
+        document.getElementById("linkWin").style.display = "block";
+      }
+      if(moveVarTwo > 80){
+        document.getElementById("kirbyWin").style.display = "block";
+      }
+      return;
+
+    }
+
   if(event.keyCode == 90){
-    moveVarOne = moveVarOne + 4;
-    playerOne.style.left = moveVarOne + "%";
+
+    moveVarOne = moveVarOne + 2;
+    link.style.left = moveVarOne + "%";
   }
   if(event.keyCode == 39){
-    moveVarTwo = moveVarTwo + 4;
-    playerTwo.style.left = moveVarTwo + "%";
+    moveVarTwo = moveVarTwo + 2;
+    kirby.style.left = moveVarTwo + "%";
+  }
+  if(moveVarOne > 80 || moveVarTwo > 80){
+    console.log('win');
   }
 }
 
